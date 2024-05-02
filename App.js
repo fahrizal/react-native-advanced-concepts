43b809fedeeb0bcb3c3d7941911e8892d73effaa
Fahrizal Bin Fahruddin <fahrizal.fahruddin@gmail.com>
2024-05-02 19:29:48 +0800

    base code

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { launchImageLibraryAsync } from "expo-image-picker";
+import { styles } from "./App.style";
+import React, { useState } from "react";
 
 export default function App() {
+  const [imageURIList, setImageURIList] = useState([]);
+
+  async function pickImage() {
+    const image = await launchImageLibraryAsync();
+
+    if (image.canceled) {
+      alert("Image picker canceled or failed");
+    } else {
+      setImageURIList([...imageURIList, image.assets[0].uri]);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={styles.title}>My favorite picture</Text>
+        <View style={styles.body}>
+          <ScrollView>
+            {imageURIList.map((uri, index) => (
+              <Image key={index} source={{ uri }} style={styles.image} />
+            ))}
+          </ScrollView>
+        </View>
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.button} onPress={pickImage}>
+            <Text style={styles.buttonText}>Add picture</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    paddingVertical: 10,
+    textAlign: "center",
+  },
+  body: {
+    flex: 6,
+  },
+  image: { height: 300, marginVertical: 30 },
+  footer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "black",
+    padding: 30,
+  },
+  buttonText: {
+    color: "white",
+  },
+});
